2ab96a439465b24334871e0e8512913fdd799912
Tomaszek Ktos <tomasz.wojcick@gmail.com>
2021-12-14 16:07:38 +0100

    Made new project

---
+++ cmake-build-debug/CMakeFiles/NetSim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetSim.dir/main.cpp.o"
+  "NetSim"
+  "NetSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NetSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/package.hpp
@@ -0,0 +1,10 @@
+//
+// Created by twojcick on 14.12.2021.
+//
+
+#ifndef NETSIM_PACKAGE_HPP
+#define NETSIM_PACKAGE_HPP
+
+#endif //NETSIM_PACKAGE_HPP
+
+// Ma zawierać definicję klasy Package
